459b9562b59ad04f3cb6ff374701a99a20079fdc
captproton <carl@captproton.com>
2012-07-08 12:35:21 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cleansheet::Application.config.secret_token = 'a24e42ccba0cb08c43698168a999b9ff661331438d1f61866141298db0480d616434d9dce20bb1348867bd77f4deb5d54f11b8715a99d169126b218e6a0612b2'
